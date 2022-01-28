c147b989472ba6fe011c7e81857c739725bbb822
minadedijer <mina-d@live.com>
2022-01-27 16:42:31 -0800

    with menu, not working tho

--- src/construction/buildMenu/BuildMenuViewController.java
+++ src/construction/buildMenu/BuildMenuViewController.java
@@ -36,6 +36,7 @@ public class BuildMenuViewController {
     public Button Breaker12Button;
     public Button Breaker70Button;
     public Button TransformerButton;
+    public Button PoleButton;
     public Button JumperButton;
     public Button CutoutButton;
 
@@ -136,7 +137,12 @@ public class BuildMenuViewController {
         TransformerButton.pseudoClassStateChanged(PseudoClass.getPseudoClass("current"), true);
         currentButton = TransformerButton;
     }
-
+    public void selectPoleTool() {
+        currentButton.pseudoClassStateChanged(PseudoClass.getPseudoClass("current"), false);
+        buildMenuFunctions.setBuildMenuData(ToolType.PLACE, ComponentType.POLE);
+        PoleButton.pseudoClassStateChanged(PseudoClass.getPseudoClass("current"), true);
+        currentButton = PoleButton;
+    }
     public void selectCutoutTool() {
         currentButton.pseudoClassStateChanged(PseudoClass.getPseudoClass("current"), false);
         buildMenuFunctions.setBuildMenuData(ToolType.PLACE, ComponentType.CUTOUT);
